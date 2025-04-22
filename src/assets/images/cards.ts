1a6815a8d55198ffc889a571506aaf2a8b0763a6
Jason Cabot <jasoncabot@gmail.com>
2025-04-22 21:21:47 +0100

    Moved assets to public

--- src/assets/images/cards.ts
+++ src/assets/images/cards.ts
@@ -2,7 +2,7 @@ import { CardEdition } from "../../shared";
 
 const cardImageMapping: { [key in CardEdition]: string[] } = {
   ["ff8"]: Array.from({ length: 110 }, (_, index) =>
-    import.meta.resolve(`../../assets/images/ff8/${index + 1}.png`),
+    `/assets/images/ff8/${index + 1}.png`
   ),
 };
 
@@ -11,16 +11,16 @@ export const cardImageFromKind = (kind: number, edition: CardEdition) => {
 };
 
 const numberMapping: { [key: number]: string } = {
-  1: import.meta.resolve("../../assets/images/numbers/1.png"),
-  2: import.meta.resolve("../../assets/images/numbers/2.png"),
-  3: import.meta.resolve("../../assets/images/numbers/3.png"),
-  4: import.meta.resolve("../../assets/images/numbers/4.png"),
-  5: import.meta.resolve("../../assets/images/numbers/5.png"),
-  6: import.meta.resolve("../../assets/images/numbers/6.png"),
-  7: import.meta.resolve("../../assets/images/numbers/7.png"),
-  8: import.meta.resolve("../../assets/images/numbers/8.png"),
-  9: import.meta.resolve("../../assets/images/numbers/9.png"),
-  10: import.meta.resolve("../../assets/images/numbers/10.png"),
+  1: "/assets/images/numbers/1.png",
+  2: "/assets/images/numbers/2.png",
+  3: "/assets/images/numbers/3.png",
+  4: "/assets/images/numbers/4.png",
+  5: "/assets/images/numbers/5.png",
+  6: "/assets/images/numbers/6.png",
+  7: "/assets/images/numbers/7.png",
+  8: "/assets/images/numbers/8.png",
+  9: "/assets/images/numbers/9.png",
+  10: "/assets/images/numbers/10.png",
 };
 
 export const numberImage = (number: number) => {
@@ -28,15 +28,15 @@ export const numberImage = (number: number) => {
 };
 
 const scoreMapping: { [key: number]: string } = {
-  1: import.meta.resolve("../../assets/images/scores/1.png"),
-  2: import.meta.resolve("../../assets/images/scores/2.png"),
-  3: import.meta.resolve("../../assets/images/scores/3.png"),
-  4: import.meta.resolve("../../assets/images/scores/4.png"),
-  5: import.meta.resolve("../../assets/images/scores/5.png"),
-  6: import.meta.resolve("../../assets/images/scores/6.png"),
-  7: import.meta.resolve("../../assets/images/scores/7.png"),
-  8: import.meta.resolve("../../assets/images/scores/8.png"),
-  9: import.meta.resolve("../../assets/images/scores/9.png"),
+  1: "/assets/images/scores/1.png",
+  2: "/assets/images/scores/2.png",
+  3: "/assets/images/scores/3.png",
+  4: "/assets/images/scores/4.png",
+  5: "/assets/images/scores/5.png",
+  6: "/assets/images/scores/6.png",
+  7: "/assets/images/scores/7.png",
+  8: "/assets/images/scores/8.png",
+  9: "/assets/images/scores/9.png",
 };
 
 export const scoreImage = (score: number) => {
